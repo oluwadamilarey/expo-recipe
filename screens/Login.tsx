823f1cb5f9f86f56d8f84868e452674776625c25
Abimbola David <38018952+oluwadamilarey@users.noreply.github.com>
2021-09-18 16:33:58 +0100

    linear gradient loin screen

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,27 +1,62 @@
-import React from 'react';
-import {
-    View,
-    Text,
-    TouchableOpacity
-} from 'react-native';
+import React from "react";
+import { View, Text, TouchableOpacity, ImageBackground } from "react-native";
 
-const Login = ({ navigation }) => {
+import { LinearGradient } from "expo-linear-gradient";
+import { COLORS, SIZES, FONTS, images } from "../constants";
+
+const Login = ({ navigation }: { navigation: any }) => {
+  const renderHeader = () => {
     return (
+      <>
         <View
-            style={{
-                flex: 1,
-                alignItems: 'center',
-                justifyContent: 'center'
-            }}
+          style={{
+            height: SIZES.height > 700 ? "65%" : "60%",
+          }}
         >
-            <Text>Login</Text>
-            <TouchableOpacity
-                onPress={() => navigation.replace("Home")}
+          <ImageBackground
+            source={images.loginBackground}
+            style={{
+              flex: 1,
+              justifyContent: "flex-end",
+            }}
+            resizeMode="cover"
+          >
+            <LinearGradient
+              start={{ x: 0, y: 0 }}
+              end={{ x: 0, y: 1 }}
+              colors={[COLORS.transparent, COLORS.black]}
+              style={{
+                height: 200,
+                justifyContent: "flex-end",
+                paddingHorizontal: SIZES.padding,
+              }}
             >
-                <Text>Navigate to Home</Text>
-            </TouchableOpacity>
+              <Text
+                style={{
+                  width: "80%",
+                  color: COLORS.white,
+                  ...FONTS.largeTitle,
+                  lineHeight: 45,
+                }}
+              >
+                Cooking A Delicious Food Easily
+              </Text>
+            </LinearGradient>
+          </ImageBackground>
         </View>
-    )
-}
+      </>
+    );
+  };
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: COLORS.black,
+      }}
+    >
+      {renderHeader()}
+    </View>
+  );
+};
 
 export default Login;
